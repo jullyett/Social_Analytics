e4e9c5cea0fb3ecebeed954af5e7c454739027e9
Julie <37387381+jullyett@users.noreply.github.com>
2018-10-14 21:22:55 -0400

    sentiment_analysis

---
+++ config_twitter.py
@@ -0,0 +1,4 @@
+consumer_key = 	"tUdfgiKhtohmiAiO1K9SkWUO1"
+consumer_secret = "43lgWs8yIBNO14enrf3ZkgzzidIj0nKQHXvMGRnHUl0RllW7tY"
+access_token = "122853510-la5MQFCTeyJ6IlQpMxtbhUBaEcjPdIaIH508uYSN"
+access_token_secret = "GKqsBaoKy6uaWJ3s2eHfWh9VqUi0bNZTd4YBjOZRNXcNn"
